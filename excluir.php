f9817b8ff0c23b564486910f0b8098ae4f6d1da7
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-08 09:56:46 -0300

    Consertado o código do excluir.php.

--- excluir.php
+++ excluir.php
@@ -1,10 +1,18 @@
 <?php
 // Exclusão com risco de SQL Injection e sem confirmação
-include("conexao.php");
+include("db.php");
 
 $id = $_GET["id"];
-$sql = "DELETE FROM usuarios WHERE id = $id";
-mysqli_query($conn, $sql);
 
-header("Location: index.php");
+$sql = " DELETE FROM usuarios WHERE id=$id ";
+
+if ($conn->query($sql) === true) {
+    echo "Registro excluído com sucesso.
+        <a href='read.php'>Ver registros.</a>
+        ";
+} else {
+    echo "Erro " . $sql . '<br>' . $conn->error;
+}
+$conn -> close();
+exit();
 ?>
