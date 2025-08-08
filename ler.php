0f9433c6607ea02f40600a7cc216bb4e1bd96e37
gabrielkyochi <gabriel_k_bernardes@estudante.sesisenai.org.br>
2025-08-08 09:30:27 -0300

    Alterado o nome dos arquivos de conexão e index.php e refeito o código do ler.php para ajustar os erros e colocá-lo na boa prática.

---
+++ ler.php
@@ -0,0 +1,43 @@
+<?php
+// Listagem com erro de lógica (ordem incorreta e falta de conexão)
+include("db.php");
+
+$sql = "SELECT * FROM usuarios"; 
+
+$resultado = $conn->query($sql);
+
+echo "<h1>Lista de Usuários</h1>";
+
+if($resultado->num_rows > 0) {
+
+    echo "<table border = '1'>
+        <tr>
+            <th> ID </th>
+            <th> Nome </th>
+            <th> Email </th>
+            <th> Ações </th>
+        </tr>
+    ";
+
+    while ($linha = $resultado->fetch_assoc()) {
+
+        echo "<tr>
+            <td> {$linha['id']} </td>
+            <td> {$linha['name']} </td>
+            <td> {$linha['email']} </td>
+            <td>
+                <a href='editar.php?id={row['id']}'>Editar<a>
+                <a href='delete.php?id={row['id']}'>Excluir<a>
+            </td>
+        </tr>
+        ";
+            
+}
+echo "</table>";
+} else {
+    echo "Nenhum registro encontrado.";
+}
+
+$conn -> close();
+
+echo "<a href='create.php'>Inserir novo Registro</a>";
