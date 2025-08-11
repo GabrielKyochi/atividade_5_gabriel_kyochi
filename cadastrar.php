61218f06ec6c5f2de0c3d1f8479151057cea8271
NarutoKyochi <gabrielkyochi@gmail.com>
2025-08-11 20:04:41 -0300

    Consertado o código do cadastrar.php.

--- cadastrar.php
+++ cadastrar.php
@@ -1,23 +1,48 @@
 <?php
 // Cadastro com erros de sintaxe e falta de validação
-include("conexao.php");
+include("db.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $nome = $_POST["nome"];
-    $email = $_POST["email"]
+    $email = $_POST["email"];
 
-    $sql = "INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email')";
+    $sql = " INSERT INTO usuarios (nome, email) VALUES ('$nome', '$email') ";
     $res = mysqli_query($conn, $sql);
-    if ($res) {
+
+    if ($res === true) {
         echo "Usuário cadastrado com sucesso!";
-    else
-        echo "Erro ao cadastrar!";
+    }else{
+        echo "Erro ao cadastrar!" . $sql . '<br>'. $conn->error;
+}
+    $conn->close();
 }
 
 ?>
 
-<form method="POST">
-    Nome: <input type="text" name="nome"><br>
-    Email: <input type="email" name="email"><br>
-    <input type="submit" value="Cadastrar">
-</form>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastrar</title>
+</head>
+
+<body>
+
+    <form method="POST" action="create.php">
+
+        <label for="name">Nome:</label>
+        <input type="text" name="name" required>
+
+        <label for="email">Email:</label>
+        <input type="email" name="email" required>
+
+        <input type="submit" value="Adicionar">
+
+    </form>
+
+    <a href="read.php">Ver registros.</a>
+
+</body>
+
+</html>
--- editar.php
+++ editar.php
@@ -32,7 +32,7 @@ $row = $result -> fetch_assoc();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>update</title>
+    <title>Editar</title>
 </head>
 
 <body>
